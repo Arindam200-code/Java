af7c08a53fb87fb2cd4ac105abb6c79c6cc70a17
Arinam200-code <arindam.das.23@aot.edu.in>
2026-04-22 22:59:41 +0530

    Initial commit

--- MaxStringCount_09-04-26/src/com/aot/test/MaxCount.java
+++ MaxStringCount_09-04-26/src/com/aot/test/MaxCount.java
@@ -3,12 +3,12 @@ package com.aot.test;
 import java.util.Scanner;
 
 public class MaxCount {
-	
+
 	public static int maxAquaCurtains(String str,int L) {
-		
+
 		int maxCount=0;
 		int currentCount=0;
-		
+
 		for(int i=0;i<str.length();i++) {
 			if(str.charAt(i)=='a') {
 				currentCount ++;
@@ -23,14 +23,16 @@ public class MaxCount {
 		return maxCount;
 	}
 	public static void main(String[] args) {
-		
+
 		Scanner sc= new Scanner(System.in);
 		String str=sc.nextLine();
-		
+
 		int L = sc.nextInt();
-		
+
 		int result = maxAquaCurtains(str,L);
-		System.out.println("Result:+ "+result);
+		System.out.println("Result:- "+result);
+
+		sc.close();
 	}
 
 }
